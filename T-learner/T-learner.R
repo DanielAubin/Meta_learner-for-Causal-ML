cde5ad095b237e1cddc88af4e40b737011af41c0
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-03 20:11:29 +0100

    Update T-learner.R

--- T-learner/T-learner.R
+++ T-learner/T-learner.R
@@ -1,3 +1,19 @@
+install.packages("xgboost", repos=c("http://dmlc.ml/drat/", getOption("repos")), type="source")
+vec.pac= c("SuperLearner", "gbm", "glmnet","ranger")
+
+lapply(vec.pac, require, character.only = TRUE) 
+
+
+
+
+#Learner Library:
+learners <- c( "SL.glmnet","SL.xgboost", "SL.ranger","SL.lm","SL.mean")
+
+#CV Control for the SuperLearner
+control <- SuperLearner.CV.control(V=5)
+
+
+
 T_learner <- function(df_aux,df_main,covariates){
   
   
@@ -5,12 +21,12 @@ T_learner <- function(df_aux,df_main,covariates){
   aux_0 <- df_aux[which(df_aux$d==0),]
   
   m1_mod <- SuperLearner(Y = aux_1$y, X = aux_1[,covariates], newX = df_main[,covariates], SL.library = learners,
-                         verbose = FALSE, method = "method.NNLS", family = binomial())
+                         verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m1_hat <- m1_mod$SL.predict
   
   m0_mod <- SuperLearner(Y = aux_0$y, X = aux_0[,covariates], newX = df_main[,covariates], SL.library = learners,
-                         verbose = FALSE, method = "method.NNLS", family = binomial())
+                         verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m0_hat <- m0_mod$SL.predict
   
