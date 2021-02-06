efb89fa14485acd53540e1a2af9f7aa3b56ab1e5
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-06 20:26:21 +0100

    Update DR-learner.R

--- DR-learner/DR-learner.R
+++ DR-learner/DR-learner.R
@@ -16,21 +16,24 @@ control <- SuperLearner.CV.control(V=5)
 
 DR_learner <- function(df_aux,df_main,covariates,learners){
 
-
+  # Train a classification model to get the propensity scores
   p_mod <- SuperLearner(Y = df_aux$d, X = df_aux[,covariates], newX = df_main[,covariates], SL.library = learners,
                         verbose = FALSE, method = "method.NNLS", family = binomial(),cvControl = control)
   
   p_hat <- p_mod$SL.predict
   p_hat = ifelse(p_hat<0.025, 0.025, ifelse(p_hat>.975,.975, p_hat)) # Overlap bounding
   
+  # Split the training data into treatment and control observations
   aux_1 <- df_aux[which(df_aux$d==1),]
   aux_0 <- df_aux[which(df_aux$d==0),]
   
+  # Train a regression model for the treatment observations
   m1_mod <- SuperLearner(Y = aux_1$y, X = aux_1[,covariates], newX = df_main[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m1_hat <- m1_mod$SL.predict
   
+  # Train a regression model for the control observations
   m0_mod <- SuperLearner(Y = aux_0$y, X = aux_0[,covariates], newX = df_main[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
@@ -38,7 +41,7 @@ DR_learner <- function(df_aux,df_main,covariates,learners){
 
 
 
-
+# Apply the doubly-robust estimator 
 y_mo <- (m1_hat - m0_hat) + ((df_main$d*(df_main$y -m1_hat))/p_hat) - ((1-df_main$d)*(df_main$y - m0_hat)/(1-p_hat))
 
 
