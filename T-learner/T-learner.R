424600d99fbeafc74de016d701b20dffaa0d8020
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-06 20:25:42 +0100

    Update T-learner.R

--- T-learner/T-learner.R
+++ T-learner/T-learner.R
@@ -16,20 +16,23 @@ control <- SuperLearner.CV.control(V=5)
 
 T_learner <- function(df_aux,df_main,covariates,learners){
   
-  
+  # Split the training data into treatment and control observations
   aux_1 <- df_aux[which(df_aux$d==1),]
   aux_0 <- df_aux[which(df_aux$d==0),]
   
+  # Train a regression model for the treatment observations
   m1_mod <- SuperLearner(Y = aux_1$y, X = aux_1[,covariates], newX = df_main[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m1_hat <- m1_mod$SL.predict
   
+  # Train a regression model for the control observations
   m0_mod <- SuperLearner(Y = aux_0$y, X = aux_0[,covariates], newX = df_main[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m0_hat <- m0_mod$SL.predict
   
+  # Estimate the CATE as the difference between the two models
   score_t <- m1_hat - m0_hat
   
   return(score_t)
