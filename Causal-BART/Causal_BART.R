63fa9dc422723bec60864108c2717242b89c1b7c
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-09 13:13:23 +0100

    Update Causal_BART.R

--- Causal-BART/Causal_BART.R
+++ Causal-BART/Causal_BART.R
@@ -4,30 +4,19 @@ remotes::install_github("vdorie/bartCause")
 # Fits a collection of treatment and response models using the Bayesian Additive
 # Regression Trees (BART) algorithm, producing estimates of treatment effects.
 
+Causal_BART <- function(y,d,x){
 
-bartc(response, treatment, confounders, data, subset, weights,
-      method.rsp = c("bart", "tmle", "p.weight"),
-      method.trt = c("bart", "glm", "none"),
-      estimand   = c("ate", "att", "atc"),
-      group.by = NULL,
-      commonSup.rule = c("none", "sd", "chisq"),
-      commonSup.cut  = c(NA_real_, 1, 0.05),
-      args.rsp = list(), args.trt = list(),
-      p.scoreAsCovariate = TRUE, use.ranef = TRUE, group.effects = FALSE,
-      crossvalidate = FALSE,
-      keepCall = TRUE, verbose = TRUE,
-      \dots)
+fit_bart <- bartc(y,d,x, )
+score_bart <- extract(fit_bart,type="icate")
+score_bart_m <- apply(score_bart,2,mean)
+
+      #ite.sd <- apply(score_bart, 2, sd)
+      #ite.lb <- score_bart_m - 1.96 * ite.sd
+      #ite.ub <- score_bart_m + 1.96 * ite.sd
+
+      #cate_CBART <- as.data.frame(cbind(score_bart_m,ite.lb,ite.ub))
+      #colnames(cate_CBART) <- c("pred","X5.","X95.")
+
+     return(score_bart_m)
+ 
 }
-# response:
-# A vector of the outcome variable, or a reference to such in the \code{data}
-# argument. Can be continuous or binary.
-
-# treatment:
-# A vector of the binary treatment variable, or a reference to \code{data}.
-
-# confounders:
-# A matrix or data frame of covariates to be used in estimating the treatment
-# and response model. Can also be the right-hand-side of a formula (e.g.
-# \code{x1 + x2 + ...}). The \code{data} argument will be searched if
-# supplied.
-  
