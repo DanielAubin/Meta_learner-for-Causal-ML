f3f92f4ecfe636a011d237c1a7a10c79bf09adcc
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-12 16:05:04 +0100

    Update Causal_BART.R

--- Causal-BART/Causal_BART.R
+++ Causal-BART/Causal_BART.R
@@ -4,19 +4,20 @@ remotes::install_github("vdorie/bartCause")
 # Fits a collection of treatment and response models using the Bayesian Additive
 # Regression Trees (BART) algorithm, producing estimates of treatment effects.
 
-Causal_BART <- function(y,d,x){
 
-fit_bart <- bartc(y,d,x, )
-score_bart <- extract(fit_bart,type="icate")
+Causal_BART <- function(df_aux,df_main,covariates){
+
+fit_bart <- bartc(df_aux$y,df_aux$d,df_aux[,covariates],keepTrees = TRUE)
+score_bart <- predict(fit_bart,newdata = df_main,type="icate")
 score_bart_m <- apply(score_bart,2,mean)
+ite.sd <- apply(score_bart, 2, sd)
+ite.lb <- score_bart_m - 2 * ite.sd
+ite.ub <- score_bart_m + 2 * ite.sd
 
-      #ite.sd <- apply(score_bart, 2, sd)
-      #ite.lb <- score_bart_m - 1.96 * ite.sd
-      #ite.ub <- score_bart_m + 1.96 * ite.sd
+cate_CBART <- as.data.frame(cbind(score_bart_m,ite.lb,ite.ub))
+colnames(cate_CBART) <- c("pred","X5.","X95.")
 
-      #cate_CBART <- as.data.frame(cbind(score_bart_m,ite.lb,ite.ub))
-      #colnames(cate_CBART) <- c("pred","X5.","X95.")
+return(cate_CBART)
 
-     return(score_bart_m)
- 
 }
+
