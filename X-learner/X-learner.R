7a60d7c4f90c2634a01cd8e299f1302372ccc293
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-15 14:33:41 +0100

    Update X-learner.R

--- X-learner/X-learner.R
+++ X-learner/X-learner.R
@@ -16,21 +16,22 @@ control <- SuperLearner.CV.control(V=5)
 X_learner <- function(df_aux,df_main,covariates,learners){
 
   
-  p_mod <- SuperLearner(Y = df_aux$d, X = df_aux[,covariates], newX = df_main[,covariates], SL.library = learners,
+  p_mod <- SuperLearner(Y = df_aux$d, X = df_aux[,covariates], newX = df_aux[,covariates], SL.library = learners,
                         verbose = FALSE, method = "method.NNLS", family = binomial(),cvControl = control)
   
-  p_hat <- p_mod$SL.predict
-  p_hat = ifelse(p_hat<0.025, 0.025, ifelse(p_hat>.975,.975, p_hat)) # Overlap bounding
+  # Prop-Score for df_main
+  p_hat_main <- predict(p_mod,df_main[,covariates])$pred
+  p_hat_main = ifelse(p_hat<0.025, 0.025, ifelse(p_hat>.975,.975, p_hat)) # Overlap bounding
   
   aux_1 <- df_aux[which(df_aux$d==1),]
   aux_0 <- df_aux[which(df_aux$d==0),]
   
-  m1_mod <- SuperLearner(Y = aux_1$y, X = aux_1[,covariates], newX = df_main[,covariates], SL.library = learners,
+  m1_mod <- SuperLearner(Y = aux_1$y, X = aux_1[,covariates], newX = df_aux[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m1_hat <- m1_mod$SL.predict
   
-  m0_mod <- SuperLearner(Y = aux_0$y, X = aux_0[,covariates], newX = df_main[,covariates], SL.library = learners,
+  m0_mod <- SuperLearner(Y = aux_0$y, X = aux_0[,covariates], newX = df_aux[,covariates], SL.library = learners,
                          verbose = FALSE, method = "method.NNLS",cvControl = control)
   
   m0_hat <- m0_mod$SL.predict
@@ -38,13 +39,13 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   
   
   
-  tau1 <- df_main[which(df_main$d==1),"y"] - m0_hat[which(df_main$d==1),]
+  tau1 <- df_aux[which(df_aux$d==1),"y"] - m0_hat[which(df_aux$d==1),]
   
-  tau0 <- m1_hat[which(df_main$d==0),]  -  df_main[which(df_main$d==0),"y"]
+  tau0 <- m1_hat[which(df_aux$d==0),]  -  df_aux[which(df_aux$d==0),"y"]
   
   
   a1  <- tryCatch({
-    tau1_mod <- SuperLearner(Y = tau1[which(df_main$d==1)], X = df_main[which(df_main$d==1),covariates], newX = df_main[,covariates], SL.library = learners,
+    tau1_mod <- SuperLearner(Y = tau1, X = df_aux[which(df_aux$d==1),covariates], newX = df_main[,covariates], SL.library = learners,
                              verbose = FALSE, method = "method.NNLS",cvControl = control)
     
     score_tau1 <- tau1_mod$SL.predict
@@ -54,7 +55,7 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   },error=function(e){
     
     mean_score <- mean(tau1)
-    score_tau1 <- rep.int(mean_score, times = nrow(df_main))
+    score_tau1 <- rep.int(mean_score, times = nrow(df_aux))
     a1 <- score_tau1
     return(a1)
   })
@@ -62,7 +63,7 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   score_tau1 <- a1
   
   a0  <- tryCatch({
-    tau0_mod <- SuperLearner(Y =tau0[which(df_main$d==0)], X = data[which(df_main$d==0),covariates], newX = df_main[,covariates], SL.library = learners,
+    tau0_mod <- SuperLearner(Y =tau0, X = df_aux[which(df_aux$d==0),covariates], newX = df_main[,covariates], SL.library = learners,
                              verbose = FALSE, method = "method.NNLS",cvControl = control)
     
     score_tau0 <- tau0_mod$SL.predict
@@ -72,7 +73,7 @@ X_learner <- function(df_aux,df_main,covariates,learners){
   },error=function(e){
     
     mean_score <- mean(tau0)
-    score_tau0 <- rep.int(mean_score, times = nrow(df_main))
+    score_tau0 <- rep.int(mean_score, times = nrow(df_aux))
     a0 <- score_tau0
     return(a0)
   })
