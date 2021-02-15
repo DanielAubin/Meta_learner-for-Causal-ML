e2ed72458943732d6b60cb23cf89cce5ccfb2fc5
DanielAubin <DanielAubin@users.noreply.github.com>
2021-02-15 14:30:23 +0100

    Update R-learner.R

--- R-learner/R-learner.R
+++ R-learner/R-learner.R
@@ -17,28 +17,28 @@ control <- SuperLearner.CV.control(V=5)
 R_learner <- function(df_aux,df_main,covariates,learners){
 
 # Train a classification model to get the propensity scores
-p_mod <- SuperLearner(Y = df_aux$d, X = df_aux[,covariates], newX = df_main[,covariates], SL.library = learners,
+p_mod <- SuperLearner(Y = df_aux$d, X = df_aux[,covariates], newX = df_aux[,covariates], SL.library = learners,
                       verbose = FALSE, method = "method.NNLS", family = binomial(),cvControl = control)
 
 p_hat <- p_mod$SL.predict
 p_hat = ifelse(p_hat<0.025, 0.025, ifelse(p_hat>.975,.975, p_hat)) # Overlap bounding
 
 # Train a regression model 
-m_mod <- SuperLearner(Y = df_aux$y, X = df_aux[,covariates], newX = df_main[,covariates], SL.library = learners,
+m_mod <- SuperLearner(Y = df_aux$y, X = df_aux[,covariates], newX = df_aux[,covariates], SL.library = learners,
                       verbose = FALSE, method = "method.NNLS",cvControl = control)
 
 m_hat <- m_mod$SL.predict
 
 # Apply the R-learner (residual-on-residual approach)
-y_tilde = df_main$y - m_hat
-w_tilde = df_main$d - p_hat
+y_tilde = df_aux$y - m_hat
+w_tilde = df_aux$d - p_hat
 pseudo_outcome = y_tilde/w_tilde
 
 weights = w_tilde^2
 
 
 a  <- tryCatch({
-  R_mod <- SuperLearner(Y = pseudo_outcome, X = df_main[,covariates], newX = df_main[,covariates], SL.library = learners,
+  R_mod <- SuperLearner(Y = pseudo_outcome, X = df_aux[,covariates], newX = df_main[,covariates], SL.library = learners,
                         verbose = FALSE, method = "method.NNLS",obsWeights = weights[,1],cvControl = control)
   score_R <- R_mod$SL.predict
   a <- score_R
@@ -47,7 +47,7 @@ a  <- tryCatch({
 },error=function(e){
   
   mean_score <- weighted.mean(pseudo_outcome, w = weights)
-  score_R <- rep.int(mean_score, times = nrow(df_main))
+  score_R <- rep.int(mean_score, times = nrow(df_aux))
   a <- score_R
   return(a)
 })
